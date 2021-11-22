11eaca93420f210a044963c3e68a74e4ab6db978
Tonio Ercegović <tonio.ercegovic@fer.hr>
2021-11-23 00:57:10 +0100

    task gui basics

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import tkinter as tk
 
 from db.db import Base, engine, get_db
 from db.task import Task
+from task import TaskGUI
+
+# --- Creates GUI and initializes database ---
 
 root = tk.Tk()
 
-my_labal = tk.Label(root, text="Hello world!")
-
+with get_db() as db:
+    db.add(Task(title="Test..."))
+    db.commit()
+    task = db.query(Task).first()
+    my_labal = TaskGUI(root, task)
 my_labal.pack()
 
 Base.metadata.create_all(bind=engine)
-
-with get_db() as db:
-    db.add(Task(title="Test"))
 
 root.mainloop()
